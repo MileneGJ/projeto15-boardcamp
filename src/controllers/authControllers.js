7b116d32d124bb4f8df7ae91b7c03079b026af2c
milenegj <milene.jannetti@gmail.com>
2022-07-23 11:40:39 -0500

    Post customers

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -1,12 +1,12 @@
 import connection from "../dbStrategy/database.js";
 
-export async function listCustomers() {
+export async function listCustomers(_,res) {
     try {
         const AllCustomers = await connection.query(`SELECT * FROM customers ${res.locals.condition}`)
-        res.send(AllCustomers.rows)
+        return res.send(AllCustomers.rows)
     } catch (error) {
         console.log(error)
-        res.sendStatus(500)
+        return res.sendStatus(500)
     }
 }
 
@@ -14,8 +14,14 @@ export function getOneCustomer() {
 
 }
 
-export function createCustomer() {
-
+export async function createCustomer(req,res) {
+    try {
+        await connection.query(`INSERT INTO customers (name,phone,cpf,birthday) VALUES (${req.body.name},${req.body.phone},${req.body.cpf},${req.body.birthday})`)
+        res.sendStatus(201)
+    } catch (error) {
+        console.log(error)
+        res.sendStatus(500)
+    }
 }
 
 export function modifyCustomer() {
