7d9d5661abac8bec031ba651f0f721c0d0e80d75
milenegj <milene.jannetti@gmail.com>
2022-07-23 12:27:50 -0500

    Get games with optional query

--- src/controllers/gameControllers.js
+++ src/controllers/gameControllers.js
@@ -1,7 +1,15 @@
+import connection from "../dbStrategy/database.js"
+
 export function createGame () {
 
 }
 
-export function listGames () {
-    
+export function listGames (_,res) {
+    try {
+        const AllGames = connection.query(`SELECT * FROM games ${res.locals.condition}`)
+        res.send(AllGames.rows)
+    } catch (error) {
+        console.log(error)
+        return res.sendStatus(500)        
+    }    
 }
--- src/middlewares/gameMiddlewares.js
+++ src/middlewares/gameMiddlewares.js
@@ -1,3 +1,13 @@
 export function gameVerify () {
     
 }
+
+export function getByName (req,res,next) {
+    const name = req.query.name
+    if (name.length > 0) {
+        res.locals.condition = `WHERE name ILIKE ${req.query.name}%`
+    } else {
+        res.locals.condition = ""
+    }
+    next()
+}
