efe31c3e25635de0cdd1e3ff3b276c074405fa00
milenegj <milene.jannetti@gmail.com>
2022-07-23 12:14:20 -0500

    Put customers

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -10,8 +10,19 @@ export async function listCustomers(_,res) {
     }
 }
 
-export function getOneCustomer() {
-
+export function getOneCustomer(req,res) {
+    const id = req.params.id
+    try {
+        const FoundCustomer = await connection.query(`SELECT * FROM customers WHERE id=${id}`)
+        if(FoundCustomer.rows[0]){
+            return res.send(FoundCustomer.rows[0])
+        } else {
+            return res.sendStatus(404)
+        }
+    } catch (error) {
+        console.log(error)
+        return res.sendStatus(500)
+    }
 }
 
 export async function createCustomer(req,res) {
@@ -24,6 +35,12 @@ export async function createCustomer(req,res) {
     }
 }
 
-export function modifyCustomer() {
-
+export async function modifyCustomer(req,res) {
+    const id = req.params.id
+    try {
+        await connection.query(`UPDATE customers SET name=${res.locals.newUser.name},phone=${res.locals.newUser.phone},cpf=${res.locals.newUser.cpf},birthday=${res.locals.newUser.birthday} WHERE id=${id}`)
+    } catch (error) {
+        console.log(error)
+        res.sendStatus(500)
+    }
 }
--- src/middlewares/authMiddlewares.js
+++ src/middlewares/authMiddlewares.js
@@ -1,19 +1,29 @@
 import userSchema from '../schemas/userSchema.js'
 import connection from '../dbStrategy/database.js';
 
-export async function customerVerify (req,res,next) {
+export async function customerVerify(req, res, next) {
     const validation = userSchema.validate(req.body);
-    if(validation.error){
+    if (validation.error) {
         console.log(validation.error)
         return res.send(validation.error.details[0].message).status(400)
     }
     try {
         const foundUser = await connection.query(`SELECT * from customers WHERE cpf=${req.body.cpf}`)
-        if(foundUser.rows[0]){
-            res.locals.newUser = req.body
-            next()
+        if (req.params.id) {
+            const IDarrays = foundUser.rows.map(u=>u.id)
+            if (foundUser.rows.length>1||!IDarrays.includes(req.params.id)) {
+                return res.sendStatus(409);
+            } else {
+                res.locals.newUser = req.body;
+                next()
+            }
         } else {
-            return res.sendStatus(409)
+            if (foundUser.rows[0]) {
+                return res.sendStatus(409)
+            } else {
+                res.locals.newUser = req.body;
+                next()
+            }
         }
     } catch (error) {
         console.log(error)
@@ -21,9 +31,9 @@ export async function customerVerify (req,res,next) {
     }
 }
 
-export async function getByCPF (req,res,next) {
+export async function getByCPF(req, res, next) {
     const cpf = req.query.cpf
-    if(cpf.length>0){
+    if (cpf.length > 0) {
         res.locals.condition = `WHERE cpf LIKE ${req.query.cpf}%`
     } else {
         res.locals.condition = ""
