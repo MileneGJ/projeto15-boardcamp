76b6d3d3c7be94fe9b3a72c8de95a2a19c4c2d37
milenegj <milene.jannetti@gmail.com>
2022-07-25 11:37:16 -0500

    Create rentals OK

--- src/controllers/rentalControllers.js
+++ src/controllers/rentalControllers.js
@@ -1,5 +1,18 @@
-export function createRental () {
+import connection from "../dbStrategy/database.js";
 
+export async function createRental (req,res) {
+    const rent = res.locals.newRental
+    try {
+        await connection.query(`INSERT INTO rentals 
+        (customerId,gameId,rentDate,daysRented,returnDate,originalPrice,delayFee) 
+        VALUES ($1,$2,$3,$4,$5,$6,$7)`,
+        [rent.customerId,rent.gameId,rent.rentDate,rent.daysRented,
+        rent.returnDate,rent.originalPrice,rent.delayFee])
+        return res.sendStatus(201)
+    } catch (error) {
+        console.log(error)
+        return res.sendStatus(500)
+    }
 }
 
 export function listRentals () {
--- src/middlewares/rentalMiddlewares.js
+++ src/middlewares/rentalMiddlewares.js
@@ -1,3 +1,35 @@
-export function rentalVerify () {
+import rentalSchema from '../schemas/rentalSchema.js'
+import connection from '../dbStrategy/database.js';
+
+export async function rentalVerify (req,res,next) {
+    const validation = rentalSchema.validate(req.body)
+    if(validation.error){
+        console.log(validation.error)
+        return res.send(validation.error.details[0].message).status(400);
+    }
+    try {
+        const foundGame = await connection.query('SELECT * from games WHERE id=$1',
+    [req.body.gameId])
+    const foundUser = await connection.query('SELECT * from customers WHERE id=$1',
+    [req.body.customerId])
+    if(!foundGame.rows[0]||!foundUser.rows[0]){
+        return res.send('Usuário ou jogo inexistente').status(400);
+    } else if(foundGame.rows.stockTotal<1) {
+        return res.send('Jogo sem estoque').status(400);
+    }
+     else {
+        res.locals.newRental = {
+            ...req.body,
+            rentDate: new Date(),
+            originalPrice: req.body.daysRented*foundGame.pricePerDay,
+            returnDate: null,
+            delayFee: null
+        }
+        next()
+    }
+    } catch (error) {
+        console.log(error)
+        res.sendStatus(500)
+    }
     
 }
